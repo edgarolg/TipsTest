f5c654479541866835f519f8ddbacc6fc230972e
Edgar_olg <a01567109@tec.mx>
2025-01-03 22:48:08 -0600

    esqueleto y avances

---
+++ frontend/src/components/HeaderInfo.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="header-info">
+    <button class="backButton">Atrás</button>
+    <h1 class="pageTitle">Pago de Propinas</h1>
+    <div class="spacer"></div>
+    <div class="moneyInRegister">
+      <div class="moneyInRegisterTitle">Efectivo en Caja</div>
+      <div class="moneyInRegisterContainer">
+        <h1 class="moneyInRegisterValue">$5,500.00</h1></div>
+    </div> 
+  </div>
+  <hr  style="border: 1px solid #000; margin: 20px 0;" />
+</template>
+
+<script>
+export default {
+  name: 'HeaderInfo',
+};
+</script>
+
+<style scoped>
+.header-info {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+button {
+  margin-right: 20px;
+}
+input {
+  width: 200px;
+}
+
+.backButton {
+  border: none;
+  color: #F08D81;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  width: fit-content;
+}
+.pageTitle {
+  font-size: 30px;
+  color: black;
+  font-weight: bold;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+}
+
+.moneyInRegister {
+  font-size: 20px;
+  color: black;
+  font-weight: bold;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+}
+
+.spacer {
+  flex-grow: 1;
+}
+
+.moneyInRegisterTitle {
+  font-size: 12px;
+  color: #F08D81;
+  font-weight: bold;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+}
+
+.moneyInRegisterContainer{
+  display: flex;
+  justify-content: center;
+  background-color: #E8D4D1;
+  border-radius: 10px;
+  padding: 10px;
+}
+
+.moneyInRegisterValue {
+  margin: 0%;
+  color: #ec776a;
+}
+
+</style>
---
+++ frontend/src/views/Tips.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="tips">
+    <header class="header">
+      <HeaderInfo />
+    </header>
+    <div class="main-content">
+      <section class="section ">Section 1</section>
+      <section class="section ">
+        <ValueInput />
+      </section>
+      <section class="section ">Section 3</section>
+    </div>
+    <div class="footer">
+    totales
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import HeaderInfo from '@/components/HeaderInfo.vue';
+import ValueInput from '@/components/ValueInput.vue';
+
+export default defineComponent({
+  name: 'TipsView',
+  components: {
+    HeaderInfo,
+    ValueInput,
+  },
+});
+</script>
+
+<style scoped>
+
+.tips{
+  width: 95%;
+  margin: auto;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+.header {
+  background-color: #f8f9fa;
+  padding: 1rem;
+  text-align: center;
+}
+
+.main-content {
+  display: flex;
+  flex: 1;
+}
+
+.section {
+  flex: 1;
+  padding: 1rem;
+  border: 1px solid #dee2e6;
+}
+
+.footer {
+  background-color: #f8f9fa;
+  padding: 1rem;
+  text-align: center;
+}
+</style>
